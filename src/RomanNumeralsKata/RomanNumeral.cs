e25835b182ca591977a4c0f00821b9e0b06f1994
Chris <Christine.ann.odell@gmail.com>
2012-03-11 23:00:11 +0000

    Added mappings for cases where the roman numerals have inverted characters.  The previous failing test now passes.

--- src/RomanNumeralsKata/RomanNumeral.cs
+++ src/RomanNumeralsKata/RomanNumeral.cs
@@ -10,19 +10,22 @@ namespace RomanNumeralsKata
 		
 		public RomanNumeral ()
 		{
-			_mappings.Add(1000, "M");
-			_mappings.Add(500, "D");
-			_mappings.Add(100, "C");
-			_mappings.Add(50, "L");
-			_mappings.Add(10, "X");
-			_mappings.Add(5, "V");
+			_mappings.Add (1000, "M");
+			_mappings.Add (900, "CM");
+			_mappings.Add (500, "D");
+			_mappings.Add (400, "CD");
+			_mappings.Add (100, "C");
+			_mappings.Add (90, "XC");
+			_mappings.Add (50, "L");
+			_mappings.Add (40, "XL");
+			_mappings.Add (10, "X");
+			_mappings.Add (9, "IX");
+			_mappings.Add (5, "V");
+			_mappings.Add (4, "IV");
 		}
 		
 		public string Parse (int arabicNumeral)
-		{
-			if (arabicNumeral == 4)
-				return "IV";
-						
+		{						
 			foreach (var mapping in _mappings)
 			{
 				arabicNumeral = ParseMultiples(arabicNumeral, mapping);
